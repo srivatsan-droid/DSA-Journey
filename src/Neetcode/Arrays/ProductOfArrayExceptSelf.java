fb04f8ecb776b15e5d6e5d0d7558281a270ea0a6
Srivatsan <sriramsrivatsan2001@gmail.com>
2026-02-10 21:34:22 +0530

    Day 6 Product Except Self

---
+++ src/Neetcode/Arrays/ProductOfArrayExceptSelf.java
@@ -0,0 +1,41 @@
+package Neetcode.Arrays;
+
+public class ProductOfArrayExceptSelf {
+    public static int[] productExceptSelfBrute(int nums[]) {
+        int ans[] = new int[nums.length];
+        for(int i = 0;i < nums.length;i++) {
+            int product = 1;
+            for(int j = 0;j < nums.length;j++) {
+                if(i != j) {
+                    product *= nums[j];
+                }
+            }
+            ans[i] = product;
+        }
+        return ans;
+    }
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * right;
+            right *= nums[i];
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4};
+        int ans[] = productExceptSelf(nums);
+        for(int i = 0;i < ans.length;i++) {
+            System.out.println(ans[i]+ " ");
+        }
+    }
+}
